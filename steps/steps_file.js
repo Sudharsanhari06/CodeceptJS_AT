0ff08794f91d5032d39c5f2098ff4dd307147c80
Sudharsanhari06 <sudharsanhari2006@gmail.com>
2025-07-02 17:55:38 +0530

    validate the action in table

--- steps/steps_file.js
+++ steps/steps_file.js
@@ -1,36 +1,42 @@
+const { I } = inject();
 function gridCellSelector(email, value) {
   return `//div[@role='gridcell' and text()='${email}']/following-sibling::div[text()='${value}']`;
 }
 
 
-function tableGridSelector(email, value) {
+function tableGridSelector(email,value) {
   return `//div[@role='gridcell' and text()='${email}']/preceding-sibling::div[text()='${value}']`
 }
+
+function editData(email){
+  return `//div[text()='${email}']/following-sibling::div/div/span[contains(@id,"edit-record")]`
+}
+
+function deleteData(email){
+  return `//div[text()='${email}']/following-sibling::div/div/span[contains(@id,"delete-record")]`
+}
+
 
 module.exports = function () {
   return actor({
     fillTheForm: function (formDetail) {
-      this.amOnPage('/');
+      I.amOnPage('/');
+
+      I.click({ xpath: '//div[@class="category-cards"]/descendant::h5[text()="Forms"]' })
+      I.waitForElement('//div[text()="Elements"]/..', 5)
+      I.click({ xpath: '//div[text()="Elements"]/..' })
+
+      I.waitForElement('//span[text()="Text Box"]/..', 3)
+      I.click({ xpath: '//span[text()="Text Box"]/..' })
+
+      I.fillField({ xpath: '//input[@id="userName"]' }, formDetail.name);
 
 
-
-      this.click({ xpath: '//div[@class="category-cards"]/descendant::h5[text()="Forms"]' })
-      this.waitForElement('//div[text()="Elements"]/..', 5)
-      this.click({ xpath: '//div[text()="Elements"]/..' })
-
-      this.waitForElement('//span[text()="Text Box"]/..', 3)
-      this.click({ xpath: '//span[text()="Text Box"]/..' })
+      I.fillField({ xpath: '//input[@id="userEmail"]' }, formDetail.email);
 
 
-
-      this.fillField({ xpath: '//input[@id="userName"]' }, formDetail.name);
-
-
-      this.fillField({ xpath: '//input[@id="userEmail"]' }, formDetail.email);
-
-
-      this.fillField({ xpath: '//textarea[@id="currentAddress"]' }, formDetail.currentAddress);
-      this.fillField({ xpath: '//textarea[@id="permanentAddress"]' }, formDetail.permanentAddress);
+      I.fillField({ xpath: '//textarea[@id="currentAddress"]' }, formDetail.currentAddress);
+      I.fillField({ xpath: '//textarea[@id="permanentAddress"]' }, formDetail.permanentAddress);
 
 
       if (formDetail.name.length <= 3) {
@@ -46,56 +52,106 @@ module.exports = function () {
         throw new Error('permanentAddress cannot be empty');
       }
 
-      this.click({ xpath: '//button[@id="submit"]' });
+      I.click({ xpath: '//button[@id="submit"]' });
 
-      this.waitForElement('#output', 5);
-      this.see(`Name:${formDetail.name}`, '#name');
-      this.see(`Email:${formDetail.email}`, '#email');
-      this.see(`Current Address :${formDetail.currentAddress}`, '#currentAddress');
-      this.see(`Permananet Address :${formDetail.permanentAddress}`, '#permanentAddress');
-      this.say(' Form submitted successfully.');
-      this.wait(3);
+      I.waitForElement('#output', 5);
+      I.see(`Name:${formDetail.name}`, '#name');
+      I.see(`Email:${formDetail.email}`, '#email');
+      I.see(`Current Address :${formDetail.currentAddress}`, '#currentAddress');
+      I.see(`Permananet Address :${formDetail.permanentAddress}`, '#permanentAddress');
+      I.say(' Form submitted successfully.');
+      I.wait(3);
     },
 
     fillTheTable: function (tableData) {
-      this.amOnPage('/');
-      this.click({ xpath: '//div[@class="category-cards"]/descendant::h5[text()="Forms"]' })
-      this.waitForElement('//div[text()="Elements"]/..', 5)
-      this.click({ xpath: '//div[text()="Elements"]/..' });
-      this.waitForElement('//span[text()="Web Tables"]/..', 3)
-      this.click('//span[text()="Web Tables"]/..')   //xpath
-      this.waitForElement('//button[@id="addNewRecordButton" and text()="Add"]', 3);
+      I.amOnPage('/');
+      I.click({ xpath: '//div[@class="category-cards"]/descendant::h5[text()="Forms"]' })
+      I.waitForElement('//div[text()="Elements"]/..', 5)
+      I.click({ xpath: '//div[text()="Elements"]/..' });
+      I.waitForElement('//span[text()="Web Tables"]/..', 3)
+      I.click('//span[text()="Web Tables"]/..')   //xpath
+      I.waitForElement('//button[@id="addNewRecordButton" and text()="Add"]', 3);
 
-      this.click('//button[@id="addNewRecordButton" and text()="Add"]');
+      I.click('//button[@id="addNewRecordButton" and text()="Add"]');
 
-      this.waitForElement('//div[@id="registration-form-modal"]/../..', 5)
+      I.waitForElement('//div[@id="registration-form-modal"]/../..', 5)
 
-      this.click('//button[@id="submit" and text()="Submit"]');
-      this.seeElement({css:'#firstName:invalid'})
-      this.seeElement({css:'#lastName:invalid'})
-      this.seeElement({css:'#userEmail:invalid'})
-      this.seeElement({css:'#age:invalid'})
-      this.seeElement({css:'#salary:invalid'});
-      this.seeElement({css:'#department:invalid'});
-
-
-      this.fillField('//input[@id="firstName"]', tableData.firstname);
-      this.fillField('//input[@id="lastName"]', tableData.lastname);
-      this.fillField('//input[@id="userEmail"]', tableData.email);
-      this.fillField('//input[@id="age"]', tableData.age);
-      this.fillField('//input[@id="salary"]', tableData.salary);
-      this.fillField('//input[@id="department"]', tableData.department);
+      I.click('//button[@id="submit" and text()="Submit"]');
+      I.seeElement({css:'#firstName:invalid'})
+      I.seeElement({css:'#lastName:invalid'})
+      I.seeElement({css:'#userEmail:invalid'})
+      I.seeElement({css:'#age:invalid'})
+      I.seeElement({css:'#salary:invalid'});
+      I.seeElement({css:'#department:invalid'});
 
 
-      this.click('//button[@id="submit" and text()="Submit"]')
-      this.wait(5);
-      this.see(tableData.firstname, tableGridSelector(tableData.email, tableData.firstname))
-      this.see(tableData.lastname, tableGridSelector(tableData.email, tableData.lastname))
-      this.see(tableData.email, `//div[@role='gridcell' and text()='${tableData.email}']`)
-      this.see(String(tableData.age), tableGridSelector(tableData.email, tableData.age))
-      this.see(String(tableData.salary), gridCellSelector(tableData.email, tableData.salary))
-      this.see(tableData.department, gridCellSelector(tableData.email, tableData.department))
-      this.say('Successfully insert the data and read that..');
+      I.fillField('//input[@id="firstName"]', tableData.firstname);
+      I.fillField('//input[@id="lastName"]', tableData.lastname);
+      I.fillField('//input[@id="userEmail"]', tableData.email);
+      I.fillField('//input[@id="age"]', tableData.age);
+      I.fillField('//input[@id="salary"]', tableData.salary);
+      I.fillField('//input[@id="department"]', tableData.department);
+
+
+      I.click('//button[@id="submit" and text()="Submit"]')
+      I.wait(5);
+      I.see(tableData.firstname, tableGridSelector(tableData.email, tableData.firstname))
+      I.see(tableData.lastname, tableGridSelector(tableData.email, tableData.lastname))
+      I.see(tableData.email, `//div[@role='gridcell' and text()='${tableData.email}']`)
+      I.see(String(tableData.age), tableGridSelector(tableData.email, tableData.age))
+      I.see(String(tableData.salary), gridCellSelector(tableData.email, tableData.salary))
+      I.see(tableData.department, gridCellSelector(tableData.email, tableData.department))
+
+      I.say('Successfully insert the data and read that..');
+    },
+
+    editTable:function(tableData,updateData,email){
+      I.amOnPage('/');
+      I.click({ xpath: '//div[@class="category-cards"]/descendant::h5[text()="Forms"]' })
+      I.waitForElement('//div[text()="Elements"]/..', 5)
+      I.click({ xpath: '//div[text()="Elements"]/..' });
+      I.waitForElement('//span[text()="Web Tables"]/..', 3)
+      I.click('//span[text()="Web Tables"]/..')  
+
+      I.click(editData(email));
+      I.seeInField('//*[@id="firstName"]',tableData.firstname);
+      I.seeInField('//*[@id="lastName"]',tableData.lastname);
+      I.seeInField('//input[@id="userEmail"]', tableData.email);
+      I.seeInField('//input[@id="age"]',String(tableData.age));
+      I.seeInField('//input[@id="salary"]',String(tableData.salary));
+      I.seeInField('//input[@id="department"]', tableData.department);
+      I.say('The data matches');
+
+      I.fillField('//input[@id="firstName"]', updateData.firstname);
+      I.fillField('//input[@id="lastName"]', updateData.lastname);
+      I.fillField('//input[@id="userEmail"]', updateData.email);
+      I.fillField('//input[@id="age"]', updateData.age);
+      I.fillField('//input[@id="salary"]', updateData.salary);
+      I.fillField('//input[@id="department"]', updateData.department);
+
+      I.click('//button[@id="submit" and text()="Submit"]')
+      I.wait(5);
+      I.see(updateData.firstname, tableGridSelector(updateData.email, updateData.firstname))
+      I.see(updateData.lastname, tableGridSelector(updateData.email, updateData.lastname))
+      I.see(updateData.email, `//div[@role='gridcell' and text()='${updateData.email}']`)
+      I.see(String(updateData.age), tableGridSelector(updateData.email, updateData.age))
+      I.see(String(updateData.salary), gridCellSelector(updateData.email, updateData.salary))
+      I.see(updateData.department, gridCellSelector(updateData.email, updateData.department))
+
     }
+
+,
+    deteleTableRow:function(email){
+      I.amOnPage('/');
+      I.click({ xpath: '//div[@class="category-cards"]/descendant::h5[text()="Forms"]' })
+      I.waitForElement('//div[text()="Elements"]/..', 5)
+      I.click({ xpath: '//div[text()="Elements"]/..' });
+      I.waitForElement('//span[text()="Web Tables"]/..', 3)
+      I.click('//span[text()="Web Tables"]/..')  
+      I.click(deleteData(email));
+      I.wait(3);
+    }
+
+
   });
 }
--- tests/Form_test.js
+++ tests/Form_test.js
@@ -1,22 +1,59 @@
-Feature(' the Forms');
+Feature('the Forms');
 
-Scenario('open the form element',async({I})=>{
-    let formDetail={
-        name:"sudharsa",
-        email:'sudharsan@gmail.com',
-        currentAddress:'6/56 Sivan Kovil Street',
-        permanentAddress:'6/56 Sivan Kovil Street'
-    }
+// Scenario('open the form element',async({I})=>{
+//     let formDetail={
+//         name:"sudharsa",
+//         email:'sudharsan@gmail.com',
+//         currentAddress:'6/56 Sivan Kovil Street',
+//         permanentAddress:'6/56 Sivan Kovil Street'
+//     }
 
-    // await I.fillTheForm(formDetail);
-    let tableData={
-        firstname:"sudharsan",
-        lastname:"H",
-        email:"sudharsan@gmail.com",
-        age:19,
-        salary:20000,
-        department:'Automation Testing'
-    }
-    await I.fillTheTable(tableData)
-    
+//     // await I.fillTheForm(formDetail);
+
+
+// })
+
+
+// Scenario('Add the User Data',async(I)=>{
+//     let tableData={
+//         firstname:"sudharsan",
+//         lastname:"H",
+//         email:"sudharsan@gmail.com",
+//         age:19,
+//         salary:20000,
+//         department:'Automation Testing'
+//     }
+//     await I.fillTheTable(tableData);
+// })
+
+// Scenario('Edit the User Data', ({ I }) => {
+//     let tableData = {
+//         firstname: "Kierra",
+//         lastname: "Gentry",
+//         email: "kierra@example.com",
+//         age: 29,
+//         salary: 2000,
+//         department: 'Legal'
+//     };
+
+//     let updateData = {
+//         firstname: "sudharsan",
+//         lastname: "H",
+//         email: "sudharsanhari@gmail.com",
+//         age: 19,
+//         salary: 20000,
+//         department: 'AT'
+//     };
+
+//     let email = 'kierra@example.com';
+
+//     I.editTable(tableData, updateData, email);
+// });
+
+Scenario('Delete the Table Row',({I})=>{
+    let email = 'kierra@example.com';
+    I.deteleTableRow(email)
 })
+
+
+
