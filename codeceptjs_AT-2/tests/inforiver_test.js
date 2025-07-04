9c7463f024439ce1cea2a0ec8a9784fb420861ee
Sudharsanhari06 <sudharsanhari2006@gmail.com>
2025-07-04 09:19:35 +0530

    feat:check the background color apply in all cell

--- codeceptjs_AT-2/tests/inforiver_test.js
+++ codeceptjs_AT-2/tests/inforiver_test.js
@@ -2,7 +2,7 @@ Feature('inforiver change the row color');
 const tableColor = require('../pages/inforiver/tableColor');
 
 Scenario('open page', async ({ I }) => {
-    let region = 'Americas'
+    let region = 'Europe'
     await I.amOnPage('/');
     await tableColor.openInforiver(region);
     await tableColor.verifyRowColor(region,);
